e67ba05d3cbc6d34033980d77ef774d5e8b6105a
mcsuka <miklos_csuka@yahoo.com>
2023-03-29 17:24:00 +0200

    add DB access

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,62 @@ extern crate rocket;
 
 use dashmap::DashMap;
 
-use rocket::State;
 use rocket::http::{ContentType, Status};
+use rocket::request::{self, FromRequest, Request};
+use rocket::response::status::NotFound;
 use rocket::serde::{json::Json, Deserialize};
 use rocket::tokio::time::{sleep, Duration};
+use rocket::State;
 
+use rocket_okapi::gen::OpenApiGenerator;
 use rocket_okapi::okapi::schemars;
 use rocket_okapi::okapi::schemars::JsonSchema;
+use rocket_okapi::request::{OpenApiFromRequest, RequestHeaderInput};
 use rocket_okapi::settings::UrlObject;
 use rocket_okapi::{openapi, openapi_get_routes, rapidoc::*, swagger_ui::*};
 use serde::Serialize;
+
+use rocket_db_pools::sqlx::{self, Row};
+use rocket_db_pools::Database;
+
+#[derive(Database)]
+#[database("imdb_db")]
+struct DbPool(sqlx::PgPool);
+
+impl<'r> OpenApiFromRequest<'r> for &'r DbPool {
+    fn from_request_input(
+        _gen: &mut OpenApiGenerator,
+        _name: String,
+        _required: bool,
+    ) -> rocket_okapi::Result<RequestHeaderInput> {
+        Ok(RequestHeaderInput::None)
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, JsonSchema)]
+#[serde(crate = "rocket::serde")]
+struct TitleBasics {
+    tconst: String,
+    titletype: String,
+    primarytitle: String,
+    originaltitle: String,
+    startyear: i32,
+    runtimeminutes: i32,
+    genres: String,
+    isadult: bool,
+    principals: Vec<TitlePrincipal>
+}
+
+#[derive(Debug, Deserialize, Serialize, JsonSchema)]
+#[serde(crate = "rocket::serde")]
+struct TitlePrincipal {
+    nconst: String,
+    category: String,
+    characters: String,
+    primaryname: String,
+    birthyear: i32,
+    deathyear: i32
+}
 
 #[derive(Deserialize, Serialize, JsonSchema)]
 #[serde(crate = "rocket::serde")]
@@ -23,6 +69,36 @@ struct Greeting {
 
 struct AllGreetings {
     gr: DashMap<u32, String>,
+}
+
+struct AllGreetingsGuard<'r>(&'r DashMap<u32, String>);
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for AllGreetingsGuard<'r> {
+    type Error = ();
+
+    async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, ()> {
+        // Using `State` as a request guard. Use `inner()` to get an `'r`.
+        request
+            .guard::<&State<AllGreetings>>()
+            .await
+            .map(|allgreetings| AllGreetingsGuard(&allgreetings.gr))
+
+        // Or alternatively, using `Rocket::state()`:
+        // request.rocket().state::<AllGreetings>()
+        //     .map(|allgreetings| AllGreetingsGuard(&allgreetings.gr))
+        //     .or_forward(())
+    }
+}
+
+impl<'r> OpenApiFromRequest<'r> for AllGreetingsGuard<'r> {
+    fn from_request_input(
+        _gen: &mut OpenApiGenerator,
+        _name: String,
+        _required: bool,
+    ) -> rocket_okapi::Result<RequestHeaderInput> {
+        Ok(RequestHeaderInput::None)
+    }
 }
 
 /// Returns `"Hello World!"`
@@ -51,8 +127,10 @@ fn teapot() -> (Status, (ContentType, &'static str)) {
 /// List all greetings
 #[openapi(tag = "Hello")]
 #[get("/greetings")]
-fn greetings(all_greetings: &State<AllGreetings>) -> Json<Vec<Greeting>> {
-    let greetings = all_greetings.gr.iter()
+fn greetings(all_greetings: AllGreetingsGuard) -> Json<Vec<Greeting>> {
+    let greetings = all_greetings
+        .0
+        .iter()
         .map(|entry| Greeting {
             id: *entry.key(),
             text: entry.value().to_owned(),
@@ -70,7 +148,7 @@ fn greetings(all_greetings: &State<AllGreetings>) -> Json<Vec<Greeting>> {
 fn retrieve(all_greetings: &State<AllGreetings>, id: u32) -> String {
     let x = match all_greetings.gr.get(&id) {
         Some(rref) => Some(rref.value().to_owned()),
-        None => None
+        None => None,
     };
     format!("{:?}", x)
 }
@@ -90,7 +168,10 @@ Add a new greeting, identified by the `id`
 #[openapi(tag = "Hello")]
 #[post("/", data = "<input>")]
 fn new(all_greetings: &State<AllGreetings>, input: Json<Greeting>) -> (Status, &'static str) {
-    let x = all_greetings.gr.entry(input.id).or_insert_with(|| input.text.to_owned());
+    let x = all_greetings
+        .gr
+        .entry(input.id)
+        .or_insert_with(|| input.text.to_owned());
     if x.value() == &input.text {
         (Status::Created, "")
     } else {
@@ -108,13 +189,76 @@ async fn delay(seconds: u64) -> String {
     format!("Waited for {} seconds", seconds)
 }
 
+#[openapi(tag = "Async Process")]
+#[get("/imdb/title/<id>")]
+async fn imdb_title(db: &DbPool, id: &str) -> Result<Json<TitleBasics>, NotFound<String>> {
+    println!("########### id = {id}");
+    let result = sqlx::query("SELECT * FROM title_basics WHERE tconst = $1")
+        .bind(id)
+        .fetch_one(&db.0)
+        .await
+        .and_then(|r| { 
+            println!("########## row = {:?}", r.columns()); 
+            Ok(TitleBasics {
+                tconst: r.get::<String, &str>("tconst"),
+                titletype: r.try_get::<String, &str>("titletype").unwrap_or("".to_string()),
+                primarytitle: r.try_get::<String, &str>("primarytitle").unwrap_or("".to_string()),
+                originaltitle: r.try_get::<String, &str>("originaltitle").unwrap_or("".to_string()),
+                startyear: r.try_get::<i32, &str>("startyear").unwrap_or(0),
+                runtimeminutes: r.try_get::<i32, &str>("runtimeminutes").unwrap_or(0),
+                genres: r.try_get::<String, &str>("genres").unwrap_or("".to_string()),
+                isadult: r.try_get::<bool, &str>("isadult").unwrap_or(false),
+                principals: vec![]
+            }) 
+        })
+        .ok();
+    
+    println!("########### result = {:?}", result);
+
+    match result {
+        Some(value) => {
+            let mut new_value = value;
+            let principals = 
+            sqlx::query("SELECT tp.nconst, tp.category, tp.job, tp.characters , nb.primaryname, nb.birthyear, nb.deathyear
+                FROM title_principals tp
+                JOIN name_basics nb ON nb.nconst = tp.nconst
+                WHERE tconst = $1")
+                .bind(id)
+                .fetch_all(&db.0)
+                .await
+                .and_then(|rows| { 
+                    Ok(
+                        rows.iter().map(|r| TitlePrincipal {
+                        nconst: r.get::<String, &str>("nconst"),
+                        category: r.try_get::<String, &str>("category").unwrap_or("".to_string()),
+                        characters: r.try_get::<String, &str>("characters").unwrap_or("".to_string()),
+                        primaryname: r.try_get::<String, &str>("primaryname").unwrap_or("".to_string()),
+                        birthyear: r.try_get::<i32, &str>("birthyear").unwrap_or(0),
+                        deathyear: r.try_get::<i32, &str>("deathyear").unwrap_or(0)
+                    }).collect::<Vec<TitlePrincipal>>()
+                )
+                })
+                .ok();
+            
+            if let Some(p) = principals {
+                new_value.principals = p;
+            }
+            Ok(Json(new_value))
+        }
+        None => Err(NotFound(format!("Could not find title with id {id}"))),
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .manage(AllGreetings { gr: DashMap::new() })
+        .attach(DbPool::init())
         .mount(
             "/",
-            openapi_get_routes![index, delay, greet, query, new, retrieve, teapot, greetings],
+            openapi_get_routes![
+                index, greet, query, new, retrieve, teapot, greetings, delay, imdb_title
+            ],
         )
         .mount(
             "/swagger-ui/",
